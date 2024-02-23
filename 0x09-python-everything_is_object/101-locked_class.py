3329cd98dd5df0b26c49ffbbeeaaee097e480533
kariSally <sallykarisaf@gmail.com>
2024-02-23 16:50:35 +0000

    Python- Everything is Object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This is an implementation of a class.
+
+that set restrections to the dynamically created attributes.
+"""
+
+
+class LockedClass:
+    """The implementation of my LockedClass."""
+
+    __slots__ = ("first_name")
